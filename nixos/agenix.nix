bcee71a0efc22f0855d414814b89c3517827adc7
Nathanael Robbins <nejrobbins@gmail.com>
2023-02-19 20:22:58 -0500

    HACK: fix Agenix race condition with impermanence bind mount on etc/ssh for now

--- nixos/agenix.nix
+++ nixos/agenix.nix
@@ -8,10 +8,11 @@ with lib;
       #TODO: generate these mappings automatically
       nate_user_password.file = lib.mkForce ../hosts/zephyrus/secrets/nate_user_password.age;
     };
-    identityPaths =
-      options.age.identityPaths.default ++ (filter pathExists [
-        (config.users.users.${specialArgs.username}.home + "/.ssh/id_ed2519_${config.networking.hostName}")
-      ]);
+    identityPaths = [ "/persist/etc/ssh/ssh_host_ed25519_key" ];
+#      options.age.identityPaths.default ++ (filter pathExists [
+#        (config.users.users.${specialArgs.username}.home + "/.ssh/id_ed2519_${config.networking.hostName}")
+#        "/persist/etc/ssh/ssh_host_ed25519_key"
+#      ]);
   };
 
 }
