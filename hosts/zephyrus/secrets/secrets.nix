03657dfede42b3655162be95353e777c211c39ea
Nathanael Robbins <nejrobbins@gmail.com>
2023-02-19 19:25:18 -0500

    Switch from VM SSH host key to the physical machine's SSH host key

--- hosts/zephyrus/secrets/secrets.nix
+++ hosts/zephyrus/secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
-  zephyrus_nate_user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDKadI/u6iVs4ANQEMvg7wSd3bh9X7GeeL+RqgLEY1Vj nejrobbins@gmail.com";
-  zephyrus_host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOQKEYCOgszI8QsvDpdxGpg3VF4LN3fycyVkjgpy7cFA";
+  zephyrus_nate = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDKadI/u6iVs4ANQEMvg7wSd3bh9X7GeeL+RqgLEY1Vj nejrobbins@gmail.com";
+  zephyrus_host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINa/7K6dFiNO6Gj3RgC31fttdB2a4Rf8MZ0N6fN8KAKg";
 in
   {
-    "nate_user_password.age".publicKeys = [ zephyrus_nate_user zephyrus_host ];
+    "nate_user_password.age".publicKeys = [ zephyrus_nate zephyrus_host ];
   }
