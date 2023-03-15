20612ab08288a186b4881ef840093782b38a36ed
Nathanael Robbins <nejrobbins@gmail.com>
2023-03-15 14:58:08 -0400

    Nix-community cachix

--- common/nixconf.nix
+++ common/nixconf.nix
@@ -7,7 +7,7 @@
   nix = {
     settings = {
       substituters = [ "https://robbins.cachix.org" "https://hyprland.cachix.org" "https://nix-community.cachix.org" ];
-      trusted-public-keys = [ "robbins.cachix.org-1:xkf79oODU0/Q50qXm2dGi7Dbvt6E/mNr08l3WkHR1PY=" "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+      trusted-public-keys = [ "robbins.cachix.org-1:xkf79oODU0/Q50qXm2dGi7Dbvt6E/mNr08l3WkHR1PY=" "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
       auto-optimise-store = true;
       trusted-substituters = [ "https://robbins.cachix.org" "https://hyprland.cachix.org" "https://nix-community.cachix.org" ];
     };
