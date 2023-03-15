0d8dc638f3f0f92b1ae58ff2a0ee58074d989a72
Nathanael Robbins <nejrobbins@gmail.com>
2023-03-15 13:48:23 -0400

    Hyprland cachix

--- common/nixconf.nix
+++ common/nixconf.nix
@@ -6,10 +6,10 @@
 {
   nix = {
     settings = {
-      substituters = [ "https://robbins.cachix.org" ];
-      trusted-public-keys = [ "robbins.cachix.org-1:xkf79oODU0/Q50qXm2dGi7Dbvt6E/mNr08l3WkHR1PY=" ];
+      substituters = [ "https://robbins.cachix.org" "https://hyprland.cachix.org" "https://nix-community.cachix.org" ];
+      trusted-public-keys = [ "robbins.cachix.org-1:xkf79oODU0/Q50qXm2dGi7Dbvt6E/mNr08l3WkHR1PY=" "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
       auto-optimise-store = true;
-      trusted-substituters = [ "https://robbins.cachix.org" "https://nix-community.cachix.org" ];
+      trusted-substituters = [ "https://robbins.cachix.org" "https://hyprland.cachix.org" "https://nix-community.cachix.org" ];
     };
     package = pkgs.nixUnstable;
     extraOptions = ''
