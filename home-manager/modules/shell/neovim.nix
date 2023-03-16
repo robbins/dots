35a5b4e9c69aed87a92b7a8258cd0b0f22a2e255
Nathanael Robbins <nejrobbins@gmail.com>
2023-03-16 12:20:28 -0400

    Neovim config

--- home-manager/modules/shell/neovim.nix
+++ home-manager/modules/shell/neovim.nix
@@ -9,10 +9,37 @@ in
   };
 
   config = mkIf cfg.enable {
+    programs.zsh.shellAliases = {
+      v = "neovim";
+    };
     programs.nixvim = {
       enable = true;
       vimAlias = true;
       colorschemes.gruvbox.enable = true;
+      plugins = {
+        clangd-extensions.enable = true;
+	lsp = {
+	  enable = true;
+	  servers = {
+	    clangd.enable = true;
+	    rnix-lsp.enable = true;
+	  };
+          keymaps.lspBuf = {
+            K = "hover";
+            gd = "definition";
+          };
+	};
+	lspkind.enable = true;
+	neo-tree.enable = true;
+	nix.enable = true;
+	nvim-autopairs.enable = true;
+	nvim-cmp.enable = true;
+	telescope.enable = true;
+	todo-comments.enable = true;
+	treesitter.enable = true;
+	cmp_luasnip.enable = true;
+	cmp-treesitter.enable = true;
+      };
     };
   };
 }
