ca5389656f0fc8e325ff6812fd92c9a46d763315
Nathanael Robbins <nejrobbins@gmail.com>
2023-03-18 14:13:34 -0400

    Asusd config g14

--- hosts/zephyrus/default.nix
+++ hosts/zephyrus/default.nix
@@ -65,6 +65,54 @@
   i18n.defaultLocale = "en_US.UTF-8";
   users.defaultUserShell = pkgs.zsh;
 
+  services.asusd = {
+    enable = true;
+    asusdConfig = {
+      bat_charge_limit = 60;
+    };
+    profileConfig = ''
+    active_profile = "Quiet"
+    [fan_curves.balanced]
+    enabled = false
+    
+    [fan_curves.balanced.cpu]
+    fan = "CPU"
+    pwm = [20, 25, 45, 51, 71, 86, 112, 142]
+    temp = [47, 50, 53, 57, 61, 66, 70, 98]
+    
+    [fan_curves.balanced.gpu]
+    fan = "GPU"
+    pwm = [20, 25, 45, 51, 71, 86, 112, 142]
+    temp = [47, 50, 53, 57, 61, 66, 70, 98]
+    
+    [fan_curves.performance]
+    enabled = false
+    
+    [fan_curves.performance.cpu]
+    fan = "CPU"
+    pwm = [35, 51, 71, 86, 102, 112, 142, 163]
+    temp = [20, 48, 52, 56, 60, 64, 68, 98]
+    
+    [fan_curves.performance.gpu]
+    fan = "GPU"
+    pwm = [35, 51, 71, 86, 102, 112, 142, 163]
+    temp = [20, 48, 52, 56, 60, 64, 68, 98]
+    
+    [fan_curves.quiet]
+    enabled = false
+    
+    [fan_curves.quiet.cpu]
+    fan = "CPU"
+    pwm = [10, 20, 20, 35, 35, 51, 71, 86]
+    temp = [53, 56, 56, 59, 59, 63, 67, 98]
+    
+    [fan_curves.quiet.gpu]
+    fan = "GPU"
+    pwm = [10, 20, 20, 35, 35, 51, 71, 86]
+    temp = [53, 56, 56, 59, 59, 63, 67, 98]
+    '';
+  };
+
   # Meta
   system.stateVersion = "21.05";
 }
