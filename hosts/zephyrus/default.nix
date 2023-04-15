aaa78108d47a30278f0d76cc822c94e6e151b1d6
Nathanael Robbins <nejrobbins@gmail.com>
2023-04-14 21:04:41 -0400

    supergfxd shit

--- hosts/zephyrus/default.nix
+++ hosts/zephyrus/default.nix
@@ -66,13 +66,6 @@
   i18n.defaultLocale = "en_US.UTF-8";
   users.defaultUserShell = pkgs.zsh;
 
-  services.supergfxd = {
-    enable = true;
-    settings = {
-    "vfio_enable" = false;
-    "hotplug_type" = "Asus";
-    };
-  };
   services.asusd = {
     enable = true;
     asusdConfig = {
--- nixos/modules/services/virtualization.nix
+++ nixos/modules/services/virtualization.nix
@@ -31,5 +31,18 @@ in {
 		boot.kernelParams = [ "iommu=pt" "amd_iommu=on" "vfio-pci.ids=10de:2520,10de:228e" ];
 		boot.extraModprobeConfig = "options vfio-pci.ids=10de:2520,10de:228e";
 		boot.blacklistedKernelModules = [ "nvidia" "nouveau" ];
+		  environment.etc."supergfxd.conf" = {
+    mode = "0644";
+    source = (pkgs.formats.json { }).generate "supergfxd.conf" {
+      mode = "Integrated";
+      vfio_enable = true;
+      vfio_save = false;
+      always_reboot = false;
+      no_logind = true;
+      logout_timeout_s = 180;
+      hotplug_type = "Asus";
+    };
+  };
+	 systemd.services.supergfxd.path = [ pkgs.kmod pkgs.pciutils ];
   };
 }
