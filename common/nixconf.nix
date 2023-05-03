aa8811b7b3d383af152b2cfdfc06981d41fc82b4
Nathanael Robbins <nejrobbins@gmail.com>
2023-05-02 21:17:46 -0400

    Proper experimental features

--- common/nixconf.nix
+++ common/nixconf.nix
@@ -10,11 +10,9 @@
       trusted-public-keys = [ "robbins.cachix.org-1:xkf79oODU0/Q50qXm2dGi7Dbvt6E/mNr08l3WkHR1PY=" "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
       auto-optimise-store = true;
       trusted-substituters = [ "https://robbins.cachix.org" "https://hyprland.cachix.org" "https://nix-community.cachix.org" ];
+      experimental-features = [ "nix-command" "flakes" ];
     };
     package = pkgs.nixUnstable;
-    extraOptions = ''
-      experimental-features = nix-command flakes
-    '';
     gc = {
       automatic = true;
       options = "--delete-older-than 30d";
