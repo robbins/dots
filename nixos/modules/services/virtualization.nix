20bc1120eb779e29886b0929caabead89faf2acb
Nathanael Robbins <nejrobbins@gmail.com>
2023-05-17 20:26:45 -0400

    Supergfxd

--- nixos/modules/services/virtualization.nix
+++ nixos/modules/services/virtualization.nix
@@ -47,15 +47,15 @@ in {
 	services.supergfxd = {
 	enable = true;
 	settings = ''
-	{
-  "mode": "Hybrid",
-  "vfio_enable": true,
-  "vfio_save": true,
-  "always_reboot": false,
-  "no_logind": false,
-  "logout_timeout_s": 180,
-  "hotplug_type": "None"
-  }'';
+	(
+  mode: "Hybrid",
+  vfio_enable: true,
+  vfio_save: true,
+  always_reboot: false,
+  no_logind": false,
+  logout_timeout_s: 180,
+  hotplug_type: "None"
+  )'';
   };
   };
 }
