14082e9ccca2bcfe33dd8722f1486f62eb65d0ae
Nathanael Robbins <nejrobbins@gmail.com>
2023-07-08 15:01:16 -0400

    refactor nixos module imports

--- nixos/modules/default.nix
+++ nixos/modules/default.nix
@@ -1,16 +1,16 @@
-#
-# Import all required modules to make them available everywhere
-#
+/**
+Import all my custom modules to make them available everywhere
+*/
 
 inputs@{self, ... }:
 {
-  imports = [
+  imports = map (file: ./. + "/${file}") (builtins.filter (file: file != "default.nix") (builtins.attrNames (builtins.readDir ./.)));
+/*  imports = [
     ./android
     ./hardware
     ./users.nix
     ./services
-    ./hardware
     ./documentation.nix
     ./gui
-  ];
+  ];*/
 }
