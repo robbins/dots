7aaaa8f24e44092a72958a1c938c16c7ec939fbe
Nate Robbins <nejrobbins@gmail.com>
2023-07-09 18:47:02 -0400

    macos config

--- home-manager/users/naterobbins/default.nix
+++ home-manager/users/naterobbins/default.nix
@@ -1,7 +1,18 @@
 { config, pkgs, inputs, ... }:
 {
   imports = [
+    (../../modules/shell/git.nix) inputs
   ];
+
+  modules = {
+    shell = {
+      git = {
+        enable = true;
+        userName = "Nate Robbins";
+        userEmail = "nate.robbins@trader.ca";
+      };
+    };
+  };
 
   home.packages = with pkgs; [
     iterm2
