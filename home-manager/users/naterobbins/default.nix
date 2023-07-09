b774e791de812f464e31d869ca9fa17c62b4bf25
Nate Robbins <nate.robbins@trader.ca>
2023-07-09 18:41:54 -0400

    fix errors

--- home-manager/users/naterobbins/default.nix
+++ home-manager/users/naterobbins/default.nix
@@ -1,25 +1,10 @@
 { config, pkgs, inputs, ... }:
 {
   imports = [
-    (import ../../modules/shell/zsh.nix)
-    (import ../../modules/shell/git.nix)
   ];
-
-  modules = {
-    shell = {
-      git = {
-        enable = true;
-        userName = "Nate Robbins";
-        userEmail = "nate.robbins@trader.ca";
-      };
-      zsh.enable = true;
-    };
-  };
 
   home.packages = with pkgs; [
     iterm2
-    postman
-    darwin.xcode_14_1
   ];
 
   # Meta
