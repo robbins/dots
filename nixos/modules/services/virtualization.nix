e067dce9326fe7bb2830e6b691143c731af82acc
Nathanael Robbins <nejrobbins@gmail.com>
2023-07-29 13:02:34 -0400

    Revert "TEMP: disable tpm2"
    This reverts commit 352f6685541186f5c2dcb8d8785483841b55449a.

--- nixos/modules/services/virtualization.nix
+++ nixos/modules/services/virtualization.nix
@@ -13,7 +13,7 @@ in {
     programs.dconf.enable = true;
     environment.systemPackages = with pkgs; [ virt-manager swtpm tpm2-tools libtpms looking-glass-client ];
     users.users.${specialArgs.username}.extraGroups = [ "libvirtd" "tss" "virt-viewer" "kvm" ];
-    security.tpm2.enable = false;
+    security.tpm2.enable = true;
     security.tpm2.pkcs11.enable = true;  # expose /run/current-system/sw/lib/libtpm2_pkcs11.so
     security.tpm2.tctiEnvironment.enable = true;  # TPM2TOOLS_TCTI and TPM2_PKCS11_TCTI env variables
     environment.etc = {
