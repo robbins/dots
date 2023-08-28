6833b4f8f07b4f7c03f95b284cca778396b7c0ba
Nathanael Robbins <nejrobbins@gmail.com>
2023-08-28 18:40:48 -0400

    bootloader: use amd pstate guided autonomous mode

--- hosts/linux/zephyrus/default.nix
+++ hosts/linux/zephyrus/default.nix
@@ -85,6 +85,9 @@
     '';
   };
 
+  # Guided Autonomous Mode
+  boot.kernelParams = [ "amd_pstate=guided" ];
+
   # Access built-in display backlight without root
   services.udev.extraRules = ''
     ACTION=="add", SUBSYSTEM=="backlight", KERNEL=="amdgpu_bl1", MODE="0666", RUN+="${pkgs.coreutils}/bin/chmod a+w /sys/class/backlight/%k/brightness"
