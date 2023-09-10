3358e92f618842f3723cbec2bc1335e86e5b59f0
Nathanael Robbins <nejrobbins@gmail.com>
2023-09-09 21:56:01 -0400

    Revert "temp: disable firewall"
    This reverts commit 892e19921f32406406098e5456eb64aa58202288.

--- hosts/linux/zephyrus/default.nix
+++ hosts/linux/zephyrus/default.nix
@@ -94,8 +94,6 @@
     ACTION=="add", SUBSYSTEM=="backlight", KERNEL=="amdgpu_bl0", MODE="0666", RUN+="${pkgs.coreutils}/bin/chmod a+w /sys/class/backlight/%k/brightness"
   '';
 
-  networking.firewall.enable = false;
-
   # Meta
   system.stateVersion = "21.05";
 }
