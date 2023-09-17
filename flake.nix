0f21b99cc33b6cac190caa020abbe3dfa4125366
Nathanael Robbins <nejrobbins@gmail.com>
2023-09-17 14:10:03 -0400

    Use Alejandra formatter

--- flake.nix
+++ flake.nix
@@ -60,6 +60,7 @@
 
   outputs = inputs@{ self, ... }: { # The attribute set argument to outputs taking self + all flakes specified in the inputs attribute can be referred to by 'inputs'
     mylib = import ./lib inputs;
+    formatter.x86_64-linux = inputs.nixpkgs.legacyPackages.x86_64-linux.alejandra;
     nixosConfigurations = import ./hosts "linux" inputs;
     darwinConfigurations = import ./hosts "darwin" inputs;
   };
