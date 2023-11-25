2b6746307ac2a8c871588b64ffe42193715a73ae
Nathanael Robbins <nejrobbins@gmail.com>
2023-11-25 18:01:25 -0500

    nixos: generate ssh host keys

--- nixos/modules/services/ssh.nix
+++ nixos/modules/services/ssh.nix
@@ -11,26 +11,36 @@ in {
     enable = mkOption {default = false;};
   };
   config = mkIf cfg.enable (mkMerge [
-		{
-  	  services.openssh = {
-  	    enable = true;
-  	    settings = {
-  	      PasswordAuthentication = false;
-  	    };
-  	  };
-  	}
+    {
+      services.openssh = {
+        enable = true;
+        settings = {
+          PasswordAuthentication = false;
+        };
+        hostKeys = [
+          {
+            bits = 4096;
+            path = "/etc/ssh/ssh_host_rsa_key";
+            type = "rsa";
+          }
+          {
+            path = "/etc/ssh/ssh_host_ed25519_key";
+            type = "ed25519";
+          }
+        ];
+      };
+    }
 
-		(mkIf config.modules.services.persistence.system.enable {
-			environment.persistence."${config.modules.services.persistence.system.persistenceRoot}" = {
-				files = [
+    (mkIf config.modules.services.persistence.system.enable {
+      environment.persistence."${config.modules.services.persistence.system.persistenceRoot}" = {
+        files = [
           "/etc/ssh/ssh_host_rsa_key"
           "/etc/ssh/ssh_host_rsa_key.pub"
           "/etc/ssh/ssh_host_ed25519_key"
           "/etc/ssh/ssh_host_ed25519_key.pub"
-					# TODO generate these keys with nix, need to rekey secrets so todo
-				];
-			};
-		})
+        ];
+      };
+    })
 
-	]);
+  ]);
 }
