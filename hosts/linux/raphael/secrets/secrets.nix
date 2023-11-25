c4016e59c8fa1ed3657f0a57e2ee505b3c18b1a8
Nathanael Robbins <nejrobbins@gmail.com>
2023-11-25 16:46:06 -0500

    nixos: create and enable tailscale module

--- hosts/linux/raphael/secrets/secrets.nix
+++ hosts/linux/raphael/secrets/secrets.nix
@@ -3,4 +3,5 @@ let
   raphael_host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGTd6H0NLHdbltc4KEZgz6koT6GkvWKdUdsmHcXt6ClD root@raphael";
 in {
   "nate_raphael_user_password.age".publicKeys = [ raphael_host raphael_nate ];
+  "nate_raphael_tailscale_auth_key.age".publicKeys = [ raphael_host raphael_nate ];
 }
---
+++ nixos/modules/services/tailscale.nix
@@ -0,0 +1,36 @@
+{
+  config,
+  pkgs,
+  lib,
+  specialArgs,
+  ...
+}:
+with lib; let
+  cfg = config.modules.services.tailscale;
+in {
+  options.modules.services.tailscale = {
+    enable = mkOption {default = false;};
+  };
+  config = mkIf cfg.enable (mkMerge [
+    {
+      services.tailscale = {
+        enable = true;
+	authKeyFile = config.age.secrets."${specialArgs.username}_${specialArgs.hostname}_tailscale_auth_key".path;
+	openFirewall = true;
+      };
+
+      environment.systemPackages = with pkgs; [ tailscale ];
+    }
+
+    (mkIf config.networking.firewall.enable {
+      networking.firewall = {
+        # always allow traffic from the Tailscale network
+        trustedInterfaces = [ "tailscale0" ];
+  
+        # allow SSH over the public internet
+        allowedTCPPorts = [ 22 ];
+      };
+    })
+
+  ]);
+}
