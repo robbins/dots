710851d27570a6d9161ce34ead1515af4890bde2
Nathanael Robbins <nejrobbins@gmail.com>
2023-11-25 19:35:47 -0500

    Revert "raphael: temp: sunshine"
    This reverts commit 0b68b45c74a463269bfcaa96252c828606559e8b.

--- hosts/linux/raphael/default.nix
+++ hosts/linux/raphael/default.nix
@@ -72,40 +72,6 @@
 
   users.users."root".password = "1";
 
-  # Sunshine TODO
-  environment.systemPackages = [ pkgs.sunshine ];
-
-  security.wrappers.sunshine = {
-    owner = "root";
-    group = "root";
-    capabilities = "cap_sys_admin+p";
-    source = "${pkgs.sunshine}/bin/sunshine";
-  };
-
-  systemd.user.services.sunshine = {
-    description = "sunshine";
-    wantedBy = [ "graphical-session.target" ];
-    startLimitIntervalSec = 500;
-    startLimitBurst = 5;
-    serviceConfig = {
-      ExecStart = "${config.security.wrapperDir}/sunshine";
-      Restart = "on-failure";
-      RestartSec = 5;
-    };
-  };
-
-  networking.firewall = {
-    allowedTCPPorts = [ 47984 47989 47990 48010 ];
-    allowedUDPPorts = [ 47998 47999 48000 48002 ];
-  };
-
-  boot.kernelModules = [ "uinput" ];
-  services.udev.extraRules = ''
-    KERNEL=="uinput", SUBSYSTEM=="misc", OPTIONS+="static_node=uinput", TAG+="uaccess"
-  '';
-
-  systemd.packages = with pkgs; [ sunshine ];
-
   # Meta
   system.stateVersion = "23.05";
 }
