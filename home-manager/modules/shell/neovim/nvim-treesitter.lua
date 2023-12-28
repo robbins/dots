9ef8fd5211ed6901deb941990828adbb90be33b3
Nathanael Robbins <nejrobbins@gmail.com>
2023-12-27 19:08:01 -0500

    hl

--- home-manager/modules/shell/neovim/nvim-treesitter.lua
+++ home-manager/modules/shell/neovim/nvim-treesitter.lua
@@ -4,4 +4,10 @@ treesitter.setup {
   highlight = {
     enable = true;
   }
+  indent = {
+    enable = true;
+  }
 }
+
+-- Highlight the @punctuation.bracket (curly brace) capture group with the highlight group ""
+vim.api.nvim_set_hl(0, "@punctuation.bracket", { link = "" })
