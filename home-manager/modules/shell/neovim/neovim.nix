5a384457e6bce52f75be185b0a040aefd9615fd5
Nathanael Robbins <nejrobbins@gmail.com>
2023-12-28 21:12:00 -0500

    test use nix

--- home-manager/modules/shell/neovim/neovim.nix
+++ home-manager/modules/shell/neovim/neovim.nix
@@ -30,43 +30,9 @@ local config = {
   -- The command that starts the language server
   -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
   cmd = {
-
-    -- 💀
-    'java', -- or '/path/to/java17_or_newer/bin/java'
-            -- depends on if `java` is in your $PATH env variable and if it points to the right version.
-
-    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
-    '-Dosgi.bundles.defaultStartLevel=4',
-    '-Declipse.product=org.eclipse.jdt.ls.core.product',
+    '${pkgs.jdt-language-server}/bin/jdt-language-server',
     '-Dlog.protocol=true',
-    '-Dlog.level=ALL',
     '-Xmx1g',
-    '--add-modules=ALL-SYSTEM',
-    '--add-opens', 'java.base/java.util=ALL-UNNAMED',
-    '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
-
-    '-Dosgi.sharedConfiguration.area=${pkgs.jdt-language-server}/share/config',
-    '-Dosgi.sharedConfiguration.area.readOnly=true',
-    '-Dosgi.checkConfiguration=true',
-    '-Dosgi.configuration.cascaded=true',
-
-    -- 💀
-    '-jar', '${pkgs.jdt-language-server}/share/java/plugins/org.eclipse.equinox.launcher_1.6.500.v20230717-2134.jar',
-         -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^                                       ^^^^^^^^^^^^^^
-         -- Must point to the                                                     Change this to
-         -- eclipse.jdt.ls installation                                           the actual version
-
-
-    -- 💀
-    -- '-configuration', '${pkgs.jdt-language-server}/share/config/config.ini',
-    -- '-configuration', '/tmp/config.ini',
-                    -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        ^^^^^^
-                    -- Must point to the                      Change to one of `linux`, `win` or `mac`
-                    -- eclipse.jdt.ls installation            Depending on your system.
-
-
-    -- 💀
-    -- See `data directory configuration` section in the README
     '-data', workspace_dir
   },
 
