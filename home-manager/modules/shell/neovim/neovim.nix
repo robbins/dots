b1dfdbfc863c990c9de97113d9938a0679fd500c
Nathanael Robbins <nejrobbins@gmail.com>
2023-12-28 19:00:11 -0500

    jdtls 3

--- home-manager/modules/shell/neovim/neovim.nix
+++ home-manager/modules/shell/neovim/neovim.nix
@@ -19,7 +19,78 @@ in {
       source = ./jdtls_setup.lua;
     };*/
     home.file."./.config/nvim/ftplugin/java.lua" = {
-      source = ./ftplugin-java.lua;
+      source = let
+        script = pkgs.writeText "ftplugin-java.lua" ''
+        print('hello')
+          -- See `:help vim.lsp.start_client` for an overview of the supported `config` options.
+local config = {
+  -- The command that starts the language server
+  -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
+  cmd = {
+
+    -- 💀
+    'java', -- or '/path/to/java17_or_newer/bin/java'
+            -- depends on if `java` is in your $PATH env variable and if it points to the right version.
+
+    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+    '-Dosgi.bundles.defaultStartLevel=4',
+    '-Declipse.product=org.eclipse.jdt.ls.core.product',
+    '-Dlog.protocol=true',
+    '-Dlog.level=ALL',
+    '-Xmx1g',
+    '--add-modules=ALL-SYSTEM',
+    '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+    '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+
+    -- 💀
+    '-jar', '${pkgs.jdt-language-server}/share/java/plugins/org.eclipse.equinox.launcher_1.6.500.v20230717-2134.jar',
+         -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^                                       ^^^^^^^^^^^^^^
+         -- Must point to the                                                     Change this to
+         -- eclipse.jdt.ls installation                                           the actual version
+
+
+    -- 💀
+    '-configuration', '${pkgs.jdt-language-server}/share/config/config.ini',
+    -- '-configuration', '/tmp/config.ini',
+                    -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        ^^^^^^
+                    -- Must point to the                      Change to one of `linux`, `win` or `mac`
+                    -- eclipse.jdt.ls installation            Depending on your system.
+
+
+    -- 💀
+    -- See `data directory configuration` section in the README
+    '-data', '/tmp/jdtls'
+  },
+
+  -- 💀
+  -- This is the default if not provided, you can remove it. Or adjust as needed.
+  -- One dedicated LSP server & client will be started per unique root_dir
+  root_dir = require('jdtls.setup').find_root({'.git', 'mvnw', 'gradlew'}),
+
+  -- Here you can configure eclipse.jdt.ls specific settings
+  -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+  -- for a list of options
+  settings = {
+    java = {
+    }
+  },
+
+  -- Language server `initializationOptions`
+  -- You need to extend the `bundles` with paths to jar files
+  -- if you want to use additional eclipse.jdt.ls plugins.
+  --
+  -- See https://github.com/mfussenegger/nvim-jdtls#java-debug-installation
+  --
+  -- If you don't plan on using the debugger or other eclipse.jdt.ls plugins you can remove this
+  init_options = {
+    bundles = {}
+  },
+}
+-- This starts a new client & server,
+-- or attaches to an existing client & server depending on the `root_dir`.
+require('jdtls').start_or_attach(config)
+        '';
+      in "${script}";
     };
     programs.neovim = {
       enable = true;
@@ -134,6 +205,7 @@ in {
         nixd
         lua-language-server
         jdt-language-server
+#        (writeShellScriptBin "jdtls" "exec ${jdt-language-server}/bin/jdt-language-server")
       ];
     };
   };
