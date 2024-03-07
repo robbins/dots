7917bf6361a86a740f9b6c7619f0df411c38d212
Nathanael Robbins <nejrobbins@gmail.com>
2024-03-07 00:48:31 -0500

    git: delta config

--- home-manager/modules/shell/git.nix
+++ home-manager/modules/shell/git.nix
@@ -29,7 +29,31 @@ in {
       lfs.enable = true;
       userName = cfg.userName;
       userEmail = cfg.userEmail;
-      delta.enable = true;
+      delta = {
+        enable = true;
+        options = {
+          decorations = {
+            commit-decoration-style = "bold yellow box ul";
+            file-decoration-style = "none";
+            file-style = "bold yellow ul";
+          };
+          unobtrusive-line-numbers = {
+            line-numbers = true;
+            line-numbers-minus-style = "#444444";
+            line-numbers-zero-style = "#444444";
+            line-numbers-plus-style = "#444444";
+            line-numbers-left-format = "{nm:>4}┊";
+            line-numbers-right-format = "{np:>4}│";
+            line-numbers-left-style = "blue";
+            line-numbers-right-style = "blue";
+          };
+          merge = {
+            conflictstyle = "diff3";
+          };
+          features = "decorations unobtrusive-line-numbers";
+          whitespace-error-style = "22 reverse";
+        };
+      };
       aliases = {
         "s" = "status";
         "c" = "commit";
