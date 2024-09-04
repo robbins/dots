2efe117dbb0da6249e615da16bf59b3432c1f627
Nathanael Robbins <nejrobbins@gmail.com>
2024-09-04 15:35:14 -0400

    home-manager: direnv: clean up module

--- home-manager/modules/shell/direnv.nix
+++ home-manager/modules/shell/direnv.nix
@@ -2,6 +2,7 @@
   config,
   pkgs,
   lib,
+  specialArgs,
   ...
 }:
 let
@@ -9,13 +10,21 @@ let
 in
 {
   options.modules.shell.direnv = {
-    enable = lib.mkEnableOption "enable";
+    enable = lib.mkEnableOption "Enable Direnv";
   };
 
   config = lib.mkIf cfg.enable {
     programs.direnv = {
       enable = true;
       nix-direnv.enable = true;
+      config = {
+        global = {
+          strict_env = true;
+        };
+        whitelist = {
+          prefix = [ "/home/${specialArgs.username}/src/projects" ];
+        };
+      };
     };
   };
 }
