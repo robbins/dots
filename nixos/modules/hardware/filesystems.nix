bcee373133793c8a3c8e488623daaecec4fc8242
Nathanael Robbins <nejrobbins@gmail.com>
2024-10-30 23:13:46 -0400

    zfs.latestCompatibleLinuxPackages deprecated, get that functionality back

--- nixos/modules/hardware/filesystems.nix
+++ nixos/modules/hardware/filesystems.nix
@@ -36,7 +36,18 @@ in
           package = if cfg.zfs.unstable then pkgs.zfsUnstable else pkgs.zfs;
           devNodes = "/dev/disk/by-partuuid";
         };
-        kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
+        kernelPackages = let
+          isUnstable = config.boot.zfs.package == pkgs.zfsUnstable;
+          zfsCompatibleKernelPackages = lib.filterAttrs (name: kernelPackages:
+            (builtins.match "linux_[0-9]+_[0-9]+" name) != null
+            && (builtins.tryEval kernelPackages).success
+            && ((!isUnstable && !kernelPackages.zfs.meta.broken)
+              || (isUnstable && !kernelPackages.zfs_unstable.meta.broken)
+            )) pkgs.linuxKernel.packages;
+          latestKernelPackage = lib.last 
+            (lib.sort 
+              (a: b: (lib.versionOlder a.kernel.version b.kernel.version)) (builtins.attrValues zfsCompatibleKernelPackages));
+        in latestKernelPackage;
       };
       networking.hostId = cfg.zfs.hostId;
       services.zfs = {
