0ee9bada3bdab42a70ef7c6823864ce1b5965221
Nathanael Robbins <nejrobbins@gmail.com>
2025-11-30 12:31:00 -0500

    temp: vbox

--- hosts/linux/raphael/default.nix
+++ hosts/linux/raphael/default.nix
@@ -131,6 +131,8 @@
     "vfio-pci.ids=1002:67df,1002:aaf0"
     # Intel
     "ixgbe.allow_unsupported_sfp=1"
+    # Virtualbox
+    "kvm.enable_virt_at_load=0"
   ];
   virtualisation.libvirtd = {
     enable = true;
@@ -233,6 +235,9 @@
       ];
     })
   ];
+  # Virtualbox
+  virtualisation.virtualbox.host.enable = true;
+  users.extraGroups.vboxusers.members = [ "user-with-access-to-virtualbox" ];
 
   # Meta
   system.stateVersion = "23.05";
