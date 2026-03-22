7ecb50e4fec5fb73863f2991e6bc1402fff0c9fb
Nate Robbins <nejrobbins@gmail.com>
2026-03-22 18:57:40 -0400

    bulldozer-vm: traefik: configure and route openproject
    HTTP only for now, need to configure HTTPS later

--- hosts/linux/bulldozer-vm/default.nix
+++ hosts/linux/bulldozer-vm/default.nix
@@ -59,17 +59,69 @@
 
   services.openproject = {
     enable = true;
-    host.name = "bulldozer-vm.home.arpa";
+    host = {
+      name = "bulldozer-vm.home.arpa";
+      rootPath = "/openproject";
+    };
     secrets = {
       keyBaseFile = config.age.secrets.bulldozer-vm_openproject_secret_key_base.path;
     };
+    environment.OPENPROJECT_HTTPS = "false";
   };
 
   age.secrets.bulldozer-vm_openproject_secret_key_base = {
     owner = "openproject";
     group = "openproject";
   };
-  networking.firewall.allowedTCPPorts = [ 6346 ];
+  networking.firewall.allowedTCPPorts = [ 6346 8080 80 443 ];
+
+  services.traefik = {
+    enable = true;
+    staticConfigOptions = {
+      entryPoints = {
+			  web = {
+				  address = ":80";
+					asDefault = true;
+					#http.redirections.entrypoint = {
+					#  to = "websecure";
+					#	scheme = "https";
+				  #};
+				};
+				websecure = {
+				  address = ":443";
+					asDefault = true;
+					http.tls.certResolver = "letsencrypt";
+				};
+      };
+
+			log = {
+			  level = "INFO";
+				filePath = "${config.services.traefik.dataDir}/traefik.log";
+				format = "json";
+			};
+
+			api.dashboard = true;
+			api.insecure = true;
+    };
+	  dynamicConfigOptions = {
+		  #http.middlewares.openproject-strip-prefix.stripPrefix.prefixes = "/openproject";
+		  http.routers = {
+			  openproject = {
+				  rule = "PathPrefix(`/openproject`)";
+					#entryPoints = [ "websecure" ];
+					entryPoints = [ "web" ];
+					#tls.certResolver = "letsencrypt";
+					service = "openproject";
+					#middlewares = [ "openproject-strip-prefix" ];
+				};
+      };
+			http.services = {
+			  openproject.loadBalancer.servers = [
+				  { url = "http://127.0.0.1:6346"; }
+				];
+			};
+		};
+  };
 
   # Meta
   system.stateVersion = "25.11";
