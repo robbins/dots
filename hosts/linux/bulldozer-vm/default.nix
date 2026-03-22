d78953c5fe4125d2a618f06959bf037d0b687edf
Nate Robbins <nejrobbins@gmail.com>
2026-03-22 18:57:40 -0400

    traefik

--- hosts/linux/bulldozer-vm/default.nix
+++ hosts/linux/bulldozer-vm/default.nix
@@ -69,7 +69,41 @@
     owner = "openproject";
     group = "openproject";
   };
-  networking.firewall.allowedTCPPorts = [ 6346 ];
+  networking.firewall.allowedTCPPorts = [ 6346 8080 ];
+
+  services.traefik = {
+    enable = true;
+    staticConfigOptions = {
+      entryPoints = {
+			  web = {
+				  address = ":80";
+					asDefault = true;
+					http.redirections.entrypoint = {
+					  to = "websecure";
+						scheme = "https";
+				  };
+				};
+				websecure = {
+				  address = ":443";
+					asDefault = true;
+					http.tls.certResolver = "letsencrypt";
+				};
+      };
+
+			log = {
+			  level = "INFO";
+				filePath = "${config.services.traefik.dataDir}/traefik.log";
+				format = "json";
+			};
+
+			api.dashboard = true;
+			api.insecure = true;
+    };
+	  dynamicConfigOptions = {
+		  http.routers = {};
+			http.services = {};
+		};
+  };
 
   # Meta
   system.stateVersion = "25.11";
